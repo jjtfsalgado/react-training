d4f6b506dad3819efd030af5c9bd16161ccc0cb2
José Salgado <jjtfsalgado@gmail.com>
2017-01-25 15:22:40 +0000

    Add navigation

---
+++ timerApp/app/components/countdown.jsx
@@ -0,0 +1,14 @@
+var React = require('react');
+var {Link} = require('react-router');
+
+var Countdown = React.createClass({
+  render: function () {
+    return(
+      <div>
+        <p>Countdown.jsx rendered</p>
+      </div>
+    )
+  }
+});
+
+module.exports = Countdown;
