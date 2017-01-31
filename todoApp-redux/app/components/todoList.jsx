37109709da7e4f061ce167e520dac9c165e7bcab
José Salgado <jjtfsalgado@gmail.com>
2017-01-31 17:53:01 +0000

    Implementing redux

---
+++ todoApp-redux/app/components/todoList.jsx
@@ -0,0 +1,35 @@
+var React = require('react');
+var {connect} = require('react-redux');
+var Todo = require('Todo');
+
+var TodoList = React.createClass({
+  render:function () {
+    var {todos} = this.props;
+    var renderTodos = () => {
+      if (todos.length === 0) {
+        return(
+          <p className="container__message">Nothing to do</p>
+        )
+      }
+      return todos.map((todo) => {
+        return(
+          <Todo key={todo.id} {...todo}/>
+        )
+      })
+    };
+
+    return(
+      <div>
+        {renderTodos()}
+      </div>
+    );
+  }
+});
+
+module.exports = connect(
+  (state) => {
+    return {
+      todos: state.todos
+    };
+  }
+)(TodoList);
